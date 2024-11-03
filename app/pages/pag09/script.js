fbabed10e4ce6cf551f7b98781863e8ffbe0d6a4
unknown <dehjr1990@gmail.com>
2024-11-03 00:41:06 -0300

    [facture]

---
+++ app/pages/pag09/script.js
@@ -0,0 +1,2 @@
+events.on("ready",function(){var s=scorm.loadObject("trilha1",!0),a=scorm.loadObject("trilha2",!0),t=scorm.loadObject("trilha3",!0),e=scorm.loadObject("trilha4",!0);s&&($(".btn1").addClass("visited"),$(".btn2").removeClass("block")),a&&($(".btn2").addClass("visited"),$(".btn3").removeClass("block")),t&&($(".btn3").addClass("visited"),$(".btn4").removeClass("block")),e&&($(".btn4").addClass("visited"),$(".arrow-right").removeClass("hide"))});
+//# sourceMappingURL=script.js.map
---
+++ app/pages/trilha1-01/script.js
@@ -0,0 +1,2 @@
+events.on("ready",function(){$(".flip-click").on("click",function(){$(this).addClass("visited"),$(this).removeClass("animated"),$(".flip-click.visited").length>=$(".flip-click").length&&(scorm.saveObject("trilha1",!0),$(".arrow-right").removeClass("hide"))})});
+//# sourceMappingURL=script.js.map
