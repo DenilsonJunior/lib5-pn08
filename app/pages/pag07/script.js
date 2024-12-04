9f626b088c3b1b136dcc0065fa976143ef934f7f
unknown <dehjr1990@gmail.com>
2024-12-03 21:37:16 -0300

    [INITIAL]

--- app/pages/pag07/script.js
+++ app/pages/pag07/script.js
@@ -1,2 +1,2 @@
-events.on("ready",function(){$(".botao").on("click",function(){$(this).removeClass("wow animated")})});
+events.on("ready",function(){$(".botao").on("click",function(){$(this).removeClass("wow animated"),$(this).find(".card-color").css("background-color","#117ABA")})});
 //# sourceMappingURL=script.js.map
